409d34dd627eda9ca9011b4540a2b7d46002616b
AlexeiLineitsev <alexei75chita@mail.ru>
2020-02-26 16:35:57 +0900

    Home work 5

---
+++ lesson_5/practice_5.js
@@ -0,0 +1,34 @@
+"use strict"; // Указывает то что мы используем синтаксис ES5
+//
+// //DOM
+// //Document Object Model
+//
+//
+// //Для работы в консоли
+// // $0 - текущий элемент
+// // $1 - предыдущий элемент
+// // $$('tag||class||id') - выбрать несколько тегов
+//
+//
+// var li = document.createElement('li');
+// li.innerText = 'LOREM TEXT';
+// li.className = 'hello'; //для добавления класса
+//
+// document.body.appendChild(li); // appendChild - для добавлеие в конец  и insertBefore(что вставляем,
+// // после чего вставляем) для добавления элемента полсле указанного элемента
+//
+// li.setAttribute('title', 'ТАЙТЛ'); //для добавления атрибутов
+//
+// //li.classList.add('Kakoi-to-new-class'); //для добавления класса
+// document.querySelectorAll('li')[0].classList.add('Kakoi-to-new-class');
+//
+// document.querySelectorAll('li')[0].classList.toggle('new-class'); // Добавить новый класс
+//
+// document.querySelectorAll('li')[0].classList.toggle('new-class'); // И так же убираем этот класс
+//
+// document.querySelectorAll('li')[0].classList.contains('new-class'); // Проверка на наличие класса
+
+
+
+
+
