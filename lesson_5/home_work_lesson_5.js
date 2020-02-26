8911b4a63066833645e23079c9c6fe203efbd79a
AlexeiLineitsev <alexei75chita@mail.ru>
2020-02-26 16:38:20 +0900

    Home work 5 complite

--- lesson_5/home_work_lesson_5.js
+++ lesson_5/home_work_lesson_5.js
@@ -8,91 +8,91 @@
 "use strict";
 //
 //
-var chessBoard = {
-    arrFigue: ['Л', 'К', 'C', 'Ф', 'Кор', 'Ф', 'К', 'Л'],
-    arrLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
-    createTable: function (rows, column) {
-        let table = document.createElement('table');
-        table.className = 'tableGame';
-        document.body.prepend(table);
-
-        for (let i = 0; i < column; i++) {
-            let tr = table.insertRow(i);
-
-            for (let j = 0; j < rows; j++) {
-                let td = tr.insertCell(j);
-            }
-        }
-    },
-    brushTable: function () {
-        var rows = document.querySelector('.tableGame').rows;
-
-        for (let i = 0; i < rows.length; i++) {
-            var cells = document.querySelector('.tableGame').rows[i].querySelectorAll('td');
-            for (let j = 0; j < cells.length; j++) {
-                if (i % 2 === 0 && j % 2 !== 0) {
-                    let cell = cells[j];
-                    cell.style.backgroundColor = '#21262d';
-                    cell.className = 'white';
-
-                } else if (i % 2 !== 0 && j % 2 === 0) {
-                    let cell = cells[j];
-                    cell.style.backgroundColor = '#21262d';
-                    cell.className = 'white';
-                }
-            }
-        }
-    },
-    addInfoInTable: function (countRows) {
-        document.querySelector('.tableGame').insertRow(0);
-        var rows = document.querySelector('.tableGame').rows;
-
-        for (let i = 0; i < countRows; i++) {
-            let cell = rows[0].insertCell(i);
-            cell.className = 'digits';
-            cell.textContent = i + 1;
-        }
-
-        for (let i = 0; i < countRows + 1; i++) {
-            let row = rows[i].insertCell(0);
-            row.className = 'letters';
-            if (i !== 0) {
-                row.textContent = this.arrLetters[i - 1];
-            }
-        }
-    },
-    addInfoInTableFigure: function () {
-        let row = document.querySelector('.tableGame').querySelectorAll('tr');
-        for (let i = 1; i <= rows; i++) {
-            row[rows].cells[i].textContent = this.arrFigue[i - 1];
-            row[rows - rows + 1].cells[i].textContent = this.arrFigue[i - 1];
-            row[rows - 1].cells[i].textContent = 'П';
-            row[rows - rows + 2].cells[i].textContent = 'П';
-
-        }
-    },
-    addInfoInTableImage: function () {
-        let row = document.querySelector('.tableGame').querySelectorAll('tr');
-        for (let i = 1; i <= rows; i++) {
-            row[rows-1].cells[i].className = 'img_pawn';
-            row[rows].cells[i].className = 'img_pawn';
-
-            row[rows - rows + 2].cells[i].className = 'img_pawn';
-            row[rows - rows + 1].cells[i].className = 'img_pawn';
-
-
-        }
-    }
-};
-
-
-var rows = 8;
-var cells = 8;
-
-
-chessBoard.createTable(rows, cells);
-chessBoard.brushTable();
-chessBoard.addInfoInTable(rows);
+// var chessBoard = {
+//     arrFigue: ['Л', 'К', 'C', 'Ф', 'Кор', 'Ф', 'К', 'Л'],
+//     arrLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+//     createTable: function (rows, column) {
+//         let table = document.createElement('table');
+//         table.className = 'tableGame';
+//         document.body.prepend(table);
+//
+//         for (let i = 0; i < column; i++) {
+//             let tr = table.insertRow(i);
+//
+//             for (let j = 0; j < rows; j++) {
+//                 let td = tr.insertCell(j);
+//             }
+//         }
+//     },
+//     brushTable: function () {
+//         var rows = document.querySelector('.tableGame').rows;
+//
+//         for (let i = 0; i < rows.length; i++) {
+//             var cells = document.querySelector('.tableGame').rows[i].querySelectorAll('td');
+//             for (let j = 0; j < cells.length; j++) {
+//                 if (i % 2 === 0 && j % 2 !== 0) {
+//                     let cell = cells[j];
+//                     cell.style.backgroundColor = '#21262d';
+//                     cell.className = 'white';
+//
+//                 } else if (i % 2 !== 0 && j % 2 === 0) {
+//                     let cell = cells[j];
+//                     cell.style.backgroundColor = '#21262d';
+//                     cell.className = 'white';
+//                 }
+//             }
+//         }
+//     },
+//     addInfoInTable: function (countRows) {
+//         document.querySelector('.tableGame').insertRow(0);
+//         var rows = document.querySelector('.tableGame').rows;
+//
+//         for (let i = 0; i < countRows; i++) {
+//             let cell = rows[0].insertCell(i);
+//             cell.className = 'digits';
+//             cell.textContent = i + 1;
+//         }
+//
+//         for (let i = 0; i < countRows + 1; i++) {
+//             let row = rows[i].insertCell(0);
+//             row.className = 'letters';
+//             if (i !== 0) {
+//                 row.textContent = this.arrLetters[i - 1];
+//             }
+//         }
+//     },
+//     addInfoInTableFigure: function () {
+//         let row = document.querySelector('.tableGame').querySelectorAll('tr');
+//         for (let i = 1; i <= rows; i++) {
+//             row[rows].cells[i].textContent = this.arrFigue[i - 1];
+//             row[rows - rows + 1].cells[i].textContent = this.arrFigue[i - 1];
+//             row[rows - 1].cells[i].textContent = 'П';
+//             row[rows - rows + 2].cells[i].textContent = 'П';
+//
+//         }
+//     },
+//     addInfoInTableImage: function () {
+//         let row = document.querySelector('.tableGame').querySelectorAll('tr');
+//         for (let i = 1; i <= rows; i++) {
+//             row[rows-1].cells[i].className = 'img_pawn';
+//             row[rows].cells[i].className = 'img_pawn';
+//
+//             row[rows - rows + 2].cells[i].className = 'img_pawn';
+//             row[rows - rows + 1].cells[i].className = 'img_pawn';
+//
+//
+//         }
+//     }
+// };
+//
+//
+// var rows = 8;
+// var cells = 8;
+//
+//
+// chessBoard.createTable(rows, cells);
+// chessBoard.brushTable();
+// chessBoard.addInfoInTable(rows);
 
 
 // 2. Заполнить созданную таблицу буквами
@@ -104,15 +104,4 @@ chessBoard.addInfoInTable(rows);
 
 // 3. * Заменить буквы, обозначающие фигуры картинками.
 //
-chessBoard.addInfoInTableImage();  // ---------------------------С фигурами -----------------------------------------
-
-// 4. * Оптимизировать скрипт из практики
-
-
-/*Оптимизировать работу 'practice.js'
-Вынести создание элемента из цикла(строка 9) var p = document.createElement('p');*/
-
-
-// Оптимизировать работу 'practice.js'
-// Вынести создание элемента из цикла(строка 9) var p = document.createElement('p');
-
+// chessBoard.addInfoInTableImage();  // ---------------------------С фигурами -----------------------------------------
